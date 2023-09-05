ef71a9937ff0d36af982bad8275a9f9bcabcca37
GoldenKunai <dhhollywood273@gmail.com>
2023-09-05 13:42:38 -0500

    getting ERR: sys can't find file specified

---
+++ Assignments/P01/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/mascot"
+)
+
+func main() {
+	fmt.Println(mascot.BestMascot())
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/dHughes97/4143-PLC-Hughes
-
-go 1.21.0
